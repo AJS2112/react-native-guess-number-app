82147a3c352ac2efcd7529433f5eb44602a15fec
Antonio Sucre <antonio.sucre@indikatore.com>
2021-11-02 05:51:58 -0300

    add-screen-component

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
+import StartGameScreeen from './screens/StartGameScreen';
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
+      <StartGameScreeen />
     </View>
   );
 }
