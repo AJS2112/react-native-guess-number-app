185801b472506123ffc42b3926cb6766e24c520c
Antonio Sucre <antonio.sucre@indikatore.com>
2021-11-10 18:34:06 -0300

    scroll-view-flexbox

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -69,8 +69,8 @@ const GameScreen = props => {
                     <Ionicons name="md-add" size={24} color="white" />
                 </MainButton>
             </Card>
-            <View style={styles.list}>
-                <ScrollView>
+            <View style={styles.listContainer}>
+                <ScrollView contentContainerStyle={styles.list}>
                     {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
                 </ScrollView>
             </View>
@@ -98,11 +98,17 @@ const styles = StyleSheet.create({
         marginVertical: 10,
         backgroundColor: 'white',
         flexDirection: 'row',
-        justifyContent: 'space-around'
+        justifyContent: 'space-around',
+        width: '60%'
     },
-    list: {
+    listContainer: {
         flex: 1,
         width: '80%'
+    },
+    list: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-end'
     }
 });
 
